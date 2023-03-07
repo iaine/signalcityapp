02aed521e06c69bdbbad1de58a46f23b848ee773
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2023-03-07 18:55:04 +0000

    Fixed some warnings

--- app/src/main/java/uk/ac/warwick/cim/signalApp/Models.java
+++ app/src/main/java/uk/ac/warwick/cim/signalApp/Models.java
@@ -1,6 +1,5 @@
 package uk.ac.warwick.cim.signalApp;
 
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.ScanResult;
 
 public class Models {
@@ -20,7 +19,7 @@ public class Models {
     /**
      * Function to create the loop model to provide a heuristic model
      * of in or out of the loop.
-     * @param distance
+     * @param distance - boolean to apply the distance
      */
     public void inLoopModel (Tone signalTone, boolean distance) {
         signalTone.playTone("C5", distance);
@@ -29,7 +28,7 @@ public class Models {
     /**
      * Function to create the loop model to provide a heuristic model
      * of out of the loop.
-     * @param distance
+     * @param distance - boolean to apply the distance
      */
     public void outLoopModel (Tone signalTone, boolean distance) {
         signalTone.playTone("G4", distance);
@@ -38,8 +37,8 @@ public class Models {
      *  Function to check if the device has been seen before.
      *
      * @todo do we want to change to collection to support counting items?
-     * @param repetition
-     * @param result
+     * @param repetition - repetition list
+     * @param result - scan result
      */
     public void checkRepetition (Tone signalTone, Repetition repetition, ScanResult result) {
         String device = result.getDevice().toString();
