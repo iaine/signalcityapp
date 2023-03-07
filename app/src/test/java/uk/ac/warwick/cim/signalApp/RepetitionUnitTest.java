9feea39fed2cb321f7407ec9c2c1925191369a37
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2023-03-07 18:33:32 +0000

    fixed test

--- app/src/test/java/uk/ac/warwick/cim/signalApp/RepetitionUnitTest.java
+++ app/src/test/java/uk/ac/warwick/cim/signalApp/RepetitionUnitTest.java
@@ -7,9 +7,16 @@ import static org.junit.Assert.*;
 public class RepetitionUnitTest {
 
     @Test
-    public void check_Repetition() {
+    public void check_Empty_Repetition() {
         Repetition repetition = new Repetition();
         Integer idx = repetition.checkRepetition("A4:54:23:cd");
-        assertEquals(0, idx.toString());
+        assert(idx == -1);
+    }
+    @Test
+    public void check_Repetition() {
+        Repetition repetition = new Repetition();
+        repetition.checkRepetition("A4:54:23:cd");
+        Integer idx = repetition.checkRepetition("A4:54:23:cd");
+        assert(idx == 0);
     }
 }
